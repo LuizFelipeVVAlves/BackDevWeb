80a58634bdd2c94a6c8538ec9343900f2c700399
LuizFelipeVVAlves <158202066+LuizFelipeVVAlves@users.noreply.github.com>
2025-06-18 17:31:38 -0300

    feat: Projeto Base Adicionado

---
+++ Baazar/Baazar/src/main/java/com/BaazarDevWeb/Baazar/BaazarApplication.java
@@ -0,0 +1,13 @@
+package com.BaazarDevWeb.Baazar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BaazarApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BaazarApplication.class, args);
+	}
+        int a = 33;
+}
---
+++ Baazar/Baazar/src/test/java/com/BaazarDevWeb/Baazar/BaazarApplicationTests.java
@@ -0,0 +1,13 @@
+package com.BaazarDevWeb.Baazar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BaazarApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
