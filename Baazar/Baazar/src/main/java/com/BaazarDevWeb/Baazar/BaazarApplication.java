754223435336ed4c95d558bd4789f3b93d195ad6
LuizFelipeVVAlves <158202066+LuizFelipeVVAlves@users.noreply.github.com>
2025-06-18 19:20:43 -0300

    feat: Nova Classe Empresa

--- Baazar/Baazar/src/main/java/com/BaazarDevWeb/Baazar/BaazarApplication.java
+++ Baazar/Baazar/src/main/java/com/BaazarDevWeb/Baazar/BaazarApplication.java
@@ -1,13 +1,17 @@
 package com.BaazarDevWeb.Baazar;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class BaazarApplication {
+public class BaazarApplication implements CommandLineRunner {
 
 	public static void main(String[] args) {
 		SpringApplication.run(BaazarApplication.class, args);
 	}
-        int a = 33;
+
+	public void run(String... args) throws Exception {
+
+	}
 }
---
+++ Baazar/Baazar/src/main/java/com/BaazarDevWeb/Baazar/model/Empresa.java
@@ -0,0 +1,32 @@
+package com.BaazarDevWeb.Baazar.model;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.DecimalMin;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+@Getter
+@Setter
+@NoArgsConstructor
+@ToString
+@Entity
+public class Empresa {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotEmpty(message = "O nome deve ser informado")
+    private String nome;
+
+    public Empresa(String nome){
+        this.nome = nome;
+    }
+}
