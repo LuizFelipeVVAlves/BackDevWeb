4b688ff4752a518c677c3b3cea0d178d4e622143
LuizFelipeVVAlves <158202066+LuizFelipeVVAlves@users.noreply.github.com>
2025-06-18 19:20:58 -0300

    feat: Nova classe Interação

---
+++ Baazar/Baazar/src/main/java/com/BaazarDevWeb/Baazar/model/Interacao.java
@@ -0,0 +1,38 @@
+package com.BaazarDevWeb.Baazar.model;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.DecimalMin;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+@Getter
+@Setter
+@NoArgsConstructor
+@ToString
+@Entity
+public class Interacao {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private int curtidas;
+
+    private int visualizacoes;
+
+    private Produto produto;
+
+    public Interacao(Produto produto){
+        this.curtidas = 0;
+        this.visualizacoes = 0;
+        this.produto = produto;
+    }
+
+}
