e5ca9ac502d7b5771382ed2e2b79f5f4ff16a510
LuizFelipeVVAlves <158202066+LuizFelipeVVAlves@users.noreply.github.com>
2025-06-19 14:57:03 -0300

    feat: endpoint login

--- Baazar/Baazar/src/main/java/com/BaazarDevWeb/Baazar/controller/AutenticacaoController.java
+++ Baazar/Baazar/src/main/java/com/BaazarDevWeb/Baazar/controller/AutenticacaoController.java
@@ -1,4 +1,26 @@
 package com.BaazarDevWeb.Baazar.controller;
+import com.BaazarDevWeb.Baazar.model.Usuario;
+import com.BaazarDevWeb.Baazar.service.AutenticacaoService;
+import com.BaazarDevWeb.Baazar.util.TokenResponse;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
 
+@CrossOrigin("http://localhost:5173")
+@RestController
+@RequestMapping("autenticacao")   // http://localhost:8080/autenticacao
 public class AutenticacaoController {
+
+    @Autowired
+    private AutenticacaoService autenticacaoService;
+
+    @PostMapping("login")  // http://localhost:8080/autenticacao/login
+    public TokenResponse login(@RequestBody Usuario usuario) {
+        System.out.println(usuario.getEmail() + " " + usuario.getSenha());
+        Usuario usuarioLogado = autenticacaoService.login(usuario);
+        if (usuarioLogado != null) {
+            return new TokenResponse(usuarioLogado.getId());
+        } else {
+            return new TokenResponse(0);
+        }
+    }
 }
