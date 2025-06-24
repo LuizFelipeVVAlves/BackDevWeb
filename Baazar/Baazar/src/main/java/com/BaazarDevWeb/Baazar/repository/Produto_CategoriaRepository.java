74a7d814346e8dc6973b4a90090ef67d594e550e
LuizFelipeVVAlves <158202066+LuizFelipeVVAlves@users.noreply.github.com>
2025-06-24 15:59:21 -0300

    feat: Repositório pra Produto_Categoria

---
+++ Baazar/Baazar/src/main/java/com/BaazarDevWeb/Baazar/repository/Produto_CategoriaRepository.java
@@ -0,0 +1,7 @@
+package com.BaazarDevWeb.Baazar.repository;
+
+import com.BaazarDevWeb.Baazar.model.Produto_Categoria;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface Produto_CategoriaRepository extends JpaRepository<Produto_Categoria, Long> {
+}
